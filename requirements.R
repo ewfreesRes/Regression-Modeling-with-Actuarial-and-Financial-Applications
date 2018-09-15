6097fdd0647fdf71f8c30cb94346a14c3d8a3815
Edward W. (Jed) Frees <30376204+ewfreesRes@users.noreply.github.com>
2018-09-15 18:00:54 -0500

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,10 +1,14 @@
 library(remotes)
-install.packages("copula", dependencies=TRUE)
-install.packages("psych")
-install.packages("Rcmdr", dependencies=TRUE)
-install_version("copula", "0.999-18")
+#install.packages("copula", dependencies=TRUE)
+#install.packages("psych")
+#install.packages("Rcmdr", dependencies=TRUE)
+#install_version("copula", "0.999-18")
 
 ##### Install specific package versions from CRAN #####
+install_version("copula", "0.999-18")
+install_version("psych", "1.8.4")
+install_version("actuar", "2.3-1”)
+
 
 # install_version("ggplot2", "2.2.1")
 # install_version("dplyr", "0.7.4")
