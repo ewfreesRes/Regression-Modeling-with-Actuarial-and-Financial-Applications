fcb1ec609b6887bebd3afecfaab61007e7768ed0
Edward W. (Jed) Frees <30376204+ewfreesRes@users.noreply.github.com>
2018-09-16 19:23:39 -0500

    Remove comments

--- requirements.R
+++ requirements.R
@@ -10,11 +10,6 @@ install_version("psych", "1.8.4")
 install_version("actuar", "2.3-1”)
 
 
-# install_version("ggplot2", "2.2.1")
-# install_version("dplyr", "0.7.4")
-# install_version("tidyr", "0.8.0")
-# install_version("purrr", "0.2.4")
-
 ##### Install packages in development from GitHub #####
 
 # install_github("nicholasehamilton/ggtern")
